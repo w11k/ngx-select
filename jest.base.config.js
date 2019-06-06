a22b401abffd25207898a59282c8f438aade29bb
Sascha Engmann <engmann@w11k.de>
2019-06-06 14:16:53 +0200

    fix failing test config

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupTestFrameworkScriptFile: "<rootDir>/src/setup-jest.jest.ts"
+  setupTestFrameworkScriptFile: "<rootDir>/src/setup-jest.jest.ts",
+  moduleNameMapper: {
+    "@w11k/ngx-select": "<rootDir>/dist/w11k/ngx-select",
+  }
 };
